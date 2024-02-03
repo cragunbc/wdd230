714ee18df274a726c07a8371a14b2c21abf307dd
cragunbc <bccragun@yahoo.com>
2024-02-02 18:57:38 -0700

    changes

---
+++ chamber/scripts/getdateschamber.js
@@ -0,0 +1,6 @@
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+document.querySelector('#currentYear').innerHTML = currentYear;
+document.querySelector('#lastModified').innerHTML = document.lastModified;
+
+console.log(document.lastModified);
