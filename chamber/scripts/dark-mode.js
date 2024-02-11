3985cbc47e4e28d25ebe507ece60e70f5d4bc341
cragunbc <bccragun@yahoo.com>
2024-02-10 22:52:33 -0700

    final changes

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -3,19 +3,26 @@ const main = document.querySelector("main");
 const hero = document.querySelector(".hero-img");
 const text = document.querySelector(".card");
 const head = document.querySelector("header");
+const foot = document.querySelector("footer");
 
 modeButton.addEventListener("click", () => {
     if (modeButton.textContent.includes("🕶️")) {
-        main.style.background = "black";
+        main.style.background = "gray";
         main.style.color = "black";
-        hero.style.background = "black";
-        head.style.background = "gray";
+        hero.style.background = "gray";
+        head.style.background = "black";
+        head.style.color = "white";
+        foot.style.background = "black";
+        foot.style.color = "white";
         modeButton.textContent = "💡";
     } else {
         main.style.background = "white";
         main.style.color = "#000";
         hero.style.background = "white";
-        head.style.background = "red";
+        head.style.background = "navy";
+        head.style.color = "white";
+        foot.style.background = "navy";
+        foot.style.color = "white";
         modeButton.textContent = "🕶️";
     }
 });
