03a392bfd1c5aeca0c8029ab4f724664d6970c79
cragunbc <bccragun@yahoo.com>
2024-02-17 13:00:30 -0700

    changes

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,6 @@
+let temp = 55;
+let speed = 15;
+
+function windChill(temp, speed) {
+    return 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+}
