4944635e5e3d00448f238376d49609ab17c42d10
cragunbc <bccragun@yahoo.com>
2024-02-19 17:36:23 -0700

    changes

---
+++ lesson07/scripts/getdates.js
@@ -0,0 +1,4 @@
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+document.querySelector('#currentYear').innerHTML = currentYear;
+document.querySelector('#lastModified').innerHTML = document.lastModified;
