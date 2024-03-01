a983d1d91e9eebb8d10191d32fb5f33ada99c615
cragunbc <bccragun@yahoo.com>
2024-02-29 18:16:37 -0700

    beginning stages

---
+++ scripts/form.js
@@ -0,0 +1,29 @@
+const pw1 = document.querySelector("#pwd1");
+const pw2 = document.querySelector("#pwd2");
+const formMessage = document.querySelector("#message");
+
+pw2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+    if (pw1.value !== pw2.value) {
+        formMessage.textContent = "Passwords DO NOT MATCH";
+        formMessage.style.visibility = "show";
+        pw2.style.backgroundColor = "red";
+        pw2.value = "";
+        pw2.focus();
+    } else {
+        formMessage.style.display = "none";
+        pw2.style.backgroundColor = "#fff";
+        pw2.style.color = "#000";
+    }
+}
+
+const rangeValue = document.querySelector("#rangevalue");
+const range = document.querySelector("#pageRat");
+
+range.addEventListener('change', dispalyRatingValue);
+range.addEventListener('input', dispalyRatingValue);
+
+function dispalyRatingValue() {
+    rangeValue.innerHTML = range.value;
+}
