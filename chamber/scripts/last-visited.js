593ada3ff47dfce103dd08e42f0d11230901c19f
cragunbc <bccragun@yahoo.com>
2024-03-02 12:20:16 -0700

    Discover Form

--- chamber/scripts/last-visited.js
+++ chamber/scripts/last-visited.js
@@ -1,27 +1,22 @@
-const todaysDate = new Date();
+const todaysDate = Date.now();
 const visitedInfo = document.querySelector("#visited");
 let visits = Number(window.localStorage.getItem("lastVisited-ls")) || 0;
 visits++;
 localStorage.setItem("lastVisited-ls", visits);
 
-let storedDate = Number(window.localStorage.getItem("lastVisited-date"));
-const date1 = new Date("2024-02-23");
-const date2 = new Date("2024-02-24");
-
-
+let storedDate = window.localStorage.getItem("lastVisited-date");
 
 if (!storedDate) {
-   localStorage.setItem("lastVisited-date", date1)
-   storedDate = Number(window.localStorage.getItem("lastVisited-date"));
+   storedDate = window.localStorage.setItem("lastVisited-date", Date.now());
+   visitedInfo.textContent = "This is your first visit!";
 } else {
-    const dateDiff = date2 - storedDate;
+    const dateDiff = todaysDate - storedDate;
     const dateDiffInDays = dateDiff / (1000 * 60 * 60 * 24);
     console.log(dateDiffInDays);
-    if (dateDiffInDays !== 0 && dateDiffInDays < 2) {
+    if (dateDiffInDays >= 0 && dateDiffInDays < 1) {
         visitedInfo.textContent = "Back so soon! Awesome!";
-    } else if (dateDiffInDays > 1){
-        visitedInfo.textContent = `You came back ${dateDiffInDays} days ago`;
-    } else if (dateDiffInDays === 0 ){
-        visitedInfo.textContent = "This is your first visit";
+    } else {
+        visitedInfo.textContent = `You came back ${Math.round(dateDiffInDays)} days ago`;
     }
+    storedDate = window.localStorage.setItem("lastVisited-date", Date.now());
 }
