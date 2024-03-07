f40dca558d4020ad9246904073de5a4e337b2d4b
cragunbc <bccragun@yahoo.com>
2024-03-06 22:04:18 -0700

    changes

--- scripts/links.js
+++ scripts/links.js
@@ -14,28 +14,18 @@ getLinks();
 const displayLinks = (weeks) => {
     weeks.forEach((week) => {
         let list = document.createElement("li");
-        let assignmentName = document.createElement("p");
         let lessonWeek = document.createElement("p");
         
-        lessonWeek.textContent = `${week.lesson}`;
+        lessonWeek.textContent = `Week ${week.lesson}: `;
         week.links.forEach((link) => {
             let assignmentLink = document.createElement("a");
-            assignmentLink.setAttribute("url", link.url);
-            assignmentLink.setAttribute("title", link.title);
-            list.appendChild(assignmentLink);
+            assignmentLink.setAttribute("href", link.url);
+            assignmentLink.textContent = `${link.title} | `;
+            lessonWeek.appendChild(assignmentLink);
+            list.appendChild(lessonWeek);
             console.log(link);
         });
-        // assignmentName.textContent = `${week.links.title}`;
-        // assignmentLink.setAttribute = ("name", `${week.links.title}`);
-        // assignmentLink.setAttribute = ("url", `${week.links.url}`);
-        // assignmentLink.textContent = `${week.links.title}`;
-        // let completeLink = `${lessonWeek}: ${assignmentName}`;
 
-        list.innerHTML = `Week ${week.lesson}:`;
-
-        // list.appendChild(lessonWeek);
-        // list.appendChild(assignmentName);
-        // list.appendChild(assignmentLink);
         assignments.appendChild(list);
     });
 }
