f544e4680861c3ad107fdc7a70259cba1c50b73b
cragunbc <bccragun@yahoo.com>
2024-03-08 20:33:52 -0700

    changes

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,43 @@
+const url = "https://cragunbc.github.io/wdd230/chamber/data/members.json"
+const cards = document.querySelector("#members");
+
+async function getMemberData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data.companies);
+}
+
+getMemberData();
+
+const displayMembers = (companies) => {
+    companies.forEach((company) => {
+        let card = document.createElement("section");
+        let image = document.createElement("img");
+        let name = document.createElement("p");
+        let address = document.createElement("p");
+        let phone = document.createElement("p");
+        let websiterul = document.createElement("p");
+        let membership = document.createElement("p");
+        let add_info = document.createElement("p");
+
+        name.textContent = company.name;
+        image.setAttribute("src", company.image);
+        image.setAttribute("alt", `Logo for ${company.name}`);
+        image.setAttribute("loading", "lazy");
+        address.textContent = company.address;
+        phone.textContent = company.phone;
+        websiterul.textContent = company.website;
+        membership.textContent = `Membership Level: ${company.membership_level}`;
+        add_info.textContent = `Additional Info: ${company.additional_info}`;
+
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(websiterul);
+        card.appendChild(membership);
+        card.appendChild(add_info);
+
+        cards.appendChild(card);
+    });
+}
