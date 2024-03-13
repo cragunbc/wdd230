46794c3a76614d6ae94c162ff87504301521abf5
cragunbc <bccragun@yahoo.com>
2024-03-12 20:59:31 -0600

    Changes

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -20,12 +20,12 @@ async function apiFetch() {
 apiFetch();
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.temp}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather.icon[0]}.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute("src", iconsrc);
     weatherIcon.setAttribute("alt", desc);
     captionDesc.textContent = `${desc}`;
 }    
     
-displayResults();
+// displayResults();
